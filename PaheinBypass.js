68e8456dc02e1c75c7a345bdee925c093ea0513b
kulokenci <kulo.kenci@gmail.com>
2023-11-03 16:12:21 +0700

    Update to version 1.1.1

--- PaheinBypass.js
+++ PaheinBypass.js
@@ -1,87 +1,169 @@
-import { platform } from 'os'
+import { execa } from 'execa';
+import { writeFileSync, unlinkSync } from "fs";
+import { platform } from 'os';
 import { URL } from "url";
 import { scrollPageToBottom } from "puppeteer-autoscroll-down";
 import puppeteer from "puppeteer";
 import cliProgress from "cli-progress";
 
 export default class PaheinBypass {
+
+    static reJsScriptEncoded = /<script[\s\S]*?>[\s\S]*?<\/script>/gi;
+    static reHideContent = /<[^>]*target="_blank" class="shortc-button small [^>]* " rel="nofollow noreferrer noopener">([^<]*)<\/[^>]*>/g;
+    static reLink = /window\.addEventListener\('load',function\(\){\(function\(\){let counter=0;let oldlet='[^']+';var [^=]+=([^}]+})/;
     
-    constructor (downloadUrl) {
-        if (this.isValidUrl(downloadUrl)){
+    constructor (paheUrl) {
+        if (this.isValidUrl(paheUrl)){
+            this.paheUrl = paheUrl;
+            this.browser = null;
             this.bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-            this.bar.start(100, 0);
-            this.directLink = this.bypassUrl(downloadUrl);
         }
+    }
+
+    async initializeBrowser() {
+        this.browser = await puppeteer.launch({
+            headless: false,
+            executablePath: platform() === 'win32' ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe' : '/usr/bin/google-chrome-stable',
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-accelerated-2d-canvas',
+                '--disable-gpu',
+                '--disable-background-timer-throttling',
+                '--disable-backgrounding-occluded-windows',
+                '--disable-renderer-backgrounding'
+            ]
+        });
+    }
+
+    async getAllLinks(filterHost = []) {
+        return new Promise(async (resolve, reject) => {
+            if (this.browser == null){
+                await this.initializeBrowser();
+            }
+            let resultAll = [];
+            const filterLinkByHost = (arrLink, filter) => {
+                const filteredLinks = [];
+                for (const link of arrLink) {
+                    if (filter.includes(link.host)) {
+                        filteredLinks.push(link);
+                    }
+                }
+                return filteredLinks;
+            };
+            const parsedLink = await this.parseLink(this.paheUrl);
+            const parsedLinkFiltered = filterLinkByHost(parsedLink, filterHost);
+            const linkCount = filterHost.length > 0 ? parsedLinkFiltered.length : parsedLink.length;
+            this.bar.start(linkCount, 0);
+            const bypassingLink = async _ => {
+                for (let index = 0; index < linkCount; index++) {
+                    const link = filterHost.length > 0 ? parsedLinkFiltered[index].link : parsedLink[index].link;
+                    const host = filterHost.length > 0 ? parsedLinkFiltered[index].host : parsedLink[index].host;
+                    const dlLink = await this.bypassUrl(link);
+                    resultAll.push({link: link, host: host, dlLink: dlLink});
+                    this.bar.update(index + 1);
+                }
+            };
+            await bypassingLink();
+            this.bar.update(linkCount);
+            this.bar.stop();
+            await this.browser.close();
+            resolve(resultAll);
+        });
+    }
+    
+
+    parseLink (paheUrl) {
+        return new Promise(async (resolve, reject) => {
+            let tempJsPahe = 'filepahe.js';
+            const page = await this.browser.newPage();
+            page.setDefaultNavigationTimeout(0);
+            await page.goto(paheUrl);
+            const content = await page.content();
+            let resultsHost = [];
+            let resultAll = [];
+            let resultJs = content.match(PaheinBypass.reJsScriptEncoded);
+            let match;
+            while (match = PaheinBypass.reHideContent.exec(content)) {
+                resultsHost.push(match[1]); 
+            }
+            resultJs.forEach((content) => {
+                if (content.includes("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/")) {
+                    writeFileSync(tempJsPahe, content.replace('<script>','').replace('</script>',''));
+                }
+            });
+            let resultJsPaheLink = null;
+            try {
+                await execa('node', [tempJsPahe]);
+            }catch (error) {
+                resultJsPaheLink = String(error.stderr);
+            }
+            let resultJsonLink = resultJsPaheLink.match(PaheinBypass.reLink);
+            const matchsLink = JSON.parse(resultJsonLink[1]);
+            let arrIndex = 0;
+            Object.entries(matchsLink).forEach(([key, value]) => {
+                resultAll.push({link: value.trim(), host: resultsHost[arrIndex].trim()});
+                arrIndex++;
+            });
+            unlinkSync(tempJsPahe);
+            await page.close();
+            resolve(resultAll);
+        })
     }
 
     bypassUrl (urlLink) {
         return new Promise(async (resolve, reject) => {
             try {
-                this.bar.update(10);
-                const browser = await puppeteer.launch({
-                    headless: 'new',
-                    executablePath: platform() === 'win32' ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe' : '/usr/bin/google-chrome-stable',
-                    args: [
-                        '--no-sandbox',
-                        '--disable-setuid-sandbox',
-                        '--disable-accelerated-2d-canvas',
-                        '--disable-gpu',
-                        '--disable-background-timer-throttling',
-                        '--disable-backgrounding-occluded-windows',
-                        '--disable-renderer-backgrounding'
-                    ]
-                });
-                const page = await browser.newPage();
+                const page = await this.browser.newPage();
                 page.setDefaultNavigationTimeout(0);
                 await page.goto(urlLink);
-                this.bar.update(30);
+                await page.waitForNavigation({waitUntil: 'load'});
                 const urlObject = new URL(page.url());
                 const urlHost = urlObject.host;
-                await page.waitForNavigation({ waitUntil: ['load', 'networkidle2'] });
-                this.bar.update(40);
                 if (urlHost === "intercelestial.com"){
-                    await page.$eval("img[src='https://intercelestial.com/wp-content/uploads/2022/12/ok-lets-continue.png']", (el) => {
-                        el.click()
-                    });
-                    await page.waitForNavigation();
-                    this.bar.update(60);
-                    await page.$eval("a[href='#generate']", (el) => {
-                        el.click()
-                    });
-                    await page.$eval("img[src='https://intercelestial.com/wp-content/uploads/2022/12/download.png']", (el) => {
-                        el.click()
-                    });
+                    await page.waitForSelector('#soralink-human-verif-main', { visible: true });
+                    await page.click('#soralink-human-verif-main')
+                    await page.waitForSelector("#generater", { visible: true });
+                    let elementG = await page.$("#generater");
+                    let boxG = await elementG.boundingBox();
+                    await page.mouse.click(boxG.x + boxG.width / 2, boxG.y + boxG.height / 2);
+                    await page.waitForSelector("#showlink", { visible: true });
+                    let elementSL = await page.$("#showlink");
+                    let boxSL = await elementSL.boundingBox();
+                    await page.mouse.click(boxSL.x + boxSL.width / 2, boxSL.y + boxSL.height / 2);
                 }else if (urlHost === "teknoasian.com"){
+                    // Maybe PAHE no longer uses the teknoasian.com domain for download links.
+                    /* await new Promise(r => setTimeout(r, 8000));
                     await scrollPageToBottom(page);
                     await page.$eval("img[src='https://teknoasian.com/wp-content/uploads/2023/09/ok-lets-continue.png']", (el) => {
                         el.click()
                     });
                     await page.waitForNavigation();
-                    this.bar.update(60);
                     await scrollPageToBottom(page);
                     await page.$eval("img[src='https://teknoasian.com/wp-content/uploads/2023/09/download.png']", (el) => {
                         el.click()
                     });
+                    */
+                    resolve('');
                 }
-                this.bar.update(80);
-                await new Promise(r => setTimeout(r, 8000));
                 try {
-                    const pages = await browser.pages();
+                    await new Promise(r => setTimeout(r, 3 * 1000));
+                    const pages = await this.browser.pages();
                     const content = await pages[2].content();
-                    this.bar.update(100);
-                    const regexDirectAtob = /location\.href = atob\('([^']+)'\);/gm;
-                    const urlDirect = regexDirectAtob.exec(content);
-                    this.bar.stop();
-                    browser.close();
-                    resolve(atob(urlDirect[1]));
+                    let reDirectAtob = /atob\('([^']+)'\);/;
+                    const urlDirect = content.match(reDirectAtob);
+                    const linkBypass = atob(urlDirect[1]);
+                    await pages[2].close();
+                    await page.close();
+                    resolve(linkBypass);
                 } catch (e) {
-                    this.bar.stop();
-                    browser.close();
-                    reject(e);
+                    // console.error(e);
+                    await page.close();
+                    resolve('');
                 }
             } catch (e) {
-                this.bar.stop();
-                return reject(e);
+                // console.error(e);
+                resolve('');
             }
         })
     } 
--- index.js
+++ index.js
@@ -15,11 +15,25 @@ const readLineAsync = msg => {
 }
 
 const startApp = async() => {
-    const downloadLink = await readLineAsync("* Enter Pahe.in (pahe.li) Download Link: ");
-    console.log("* Processing...");  
-    const paheBypass = new PaheinBypass(downloadLink);
-    const directLink = await paheBypass.directLink;
-    console.log("* Direct Link: " + directLink);
+    /*  
+        PAHE.in File Hosting
+        1F, GD, MG, MU, PD, SD, SND, UTB
+        1fichier.com, drive.google.com, mega.nz, megaup.net ,pixeldrain.com, send.cm, uptobox.com
+        ["1F", "GD", "MG", "MU", "PD", "SD", "SND", "UTB"]
+    */
+    const paheLink = await readLineAsync("* Enter Pahe.in (pahe.li) Download Link: ");
+    try {
+        const paheBypass = new PaheinBypass(paheLink);
+        console.log("* Finding all the download links on this page."); 
+        console.log("* Avalible host file: 1F, GD, MG, MU, PD, SD, SND, UTB")
+        const filterHost = await readLineAsync("* To filter which hosts to include, input them like this: GD, MG, etc\n  If you don't want to filter, just type 'NO' and press enter: ");
+        const filterHostArr = filterHost.toLowerCase() === 'no' ? [] : filterHost.split(',');
+        const links = await paheBypass.getAllLinks(filterHostArr);
+        console.log(`* Done here all the links`); 
+        console.info(links);
+    } catch (error) {
+        console.error(error);
+    }
     readline.close();
 }
 
